3d2028c04210e43f277ef41617c8a87c2e97bd15
Hknots <hknots99@gmail.com>
2025-04-01 12:03:25 +0200

    struct: setup access

--- src/main/kotlin/no/fintlabs/resource/server/config/SecurityConfiguration.kt
+++ src/main/kotlin/no/fintlabs/resource/server/config/SecurityConfiguration.kt
@@ -1,12 +1,18 @@
 package no.fintlabs.resource.server.config
 
+import kotlinx.coroutines.reactor.awaitSingle
+import no.fintlabs.resource.server.authentication.CorePrincipal
 import no.fintlabs.resource.server.converter.CorePrincipalConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authorization.AuthorizationDecision
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authorization.AuthorizationContext
+import reactor.core.publisher.Mono
 
 @Configuration
 @EnableWebFluxSecurity
@@ -22,7 +28,7 @@ class SecurityConfiguration(
 
     private fun fintSecurity(http: ServerHttpSecurity): SecurityWebFilterChain =
         http.authorizeExchange { exchanges ->
-            exchanges.anyExchange().authenticated()
+            exchanges.anyExchange().access(this::test)
         }.oauth2ResourceServer { oauth2 ->
             oauth2.jwt { jwt ->
                 jwt.jwtAuthenticationConverter(
@@ -30,6 +36,19 @@ class SecurityConfiguration(
                 )
             }
         }.build()
+
+    private suspend fun test(
+        monoAuthentication: Mono<Authentication>,
+        authorizationContext: AuthorizationContext
+    ): AuthorizationDecision {
+        val authentication = monoAuthentication.awaitSingle()
+
+        if (authentication is CorePrincipal && co) {
+
+        }
+
+        return AuthorizationDecision(true)
+    }
 
     private fun permitAll(http: ServerHttpSecurity): SecurityWebFilterChain =
         http.authorizeExchange { exchanges ->
