bf030a6f9ed4f5f155771f68040941806fce0237
Hknots <hknots99@gmail.com>
2025-04-01 12:17:12 +0200

    refactor: add formattedValue

--- src/main/kotlin/no/fintlabs/resource/server/enums/FintScope.kt
+++ src/main/kotlin/no/fintlabs/resource/server/enums/FintScope.kt
@@ -2,5 +2,10 @@ package no.fintlabs.resource.server.enums
 
 enum class FintScope {
     FINT_CLIENT,
-    FINT_ADAPTER
+    FINT_ADAPTER;
+
+    val formattedValue: String
+        get() = name.lowercase().replace('_', '-')
+
+
 }
