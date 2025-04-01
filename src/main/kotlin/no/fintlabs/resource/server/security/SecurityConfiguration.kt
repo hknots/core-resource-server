4f26110f3cb4e7dcfaf477d8343a170dbe720f94
Hknots <hknots99@gmail.com>
2025-04-01 10:01:54 +0200

    feat: create SecurityConfiguration

--- src/main/kotlin/no/fintlabs/resource/server/security/SecurityConfiguration.kt
+++ src/main/kotlin/no/fintlabs/resource/server/security/SecurityConfiguration.kt
@@ -1,4 +1,38 @@
 package no.fintlabs.resource.server.security
 
-class SecurityConfiguration {
+import no.fintlabs.resource.server.config.SecurityProperties
+import no.fintlabs.resource.server.converter.CorePrincipalConverter
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@EnableWebFluxSecurity
+class SecurityConfiguration(
+    private val securityProperties: SecurityProperties
+) {
+
+    @Bean
+    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return if (securityProperties.enabled) fintSecurity(http)
+        else permitAll(http)
+    }
+
+    private fun fintSecurity(http: ServerHttpSecurity): SecurityWebFilterChain =
+        http.authorizeExchange { exchanges ->
+            exchanges.anyExchange().authenticated()
+        }.oauth2ResourceServer { oauth2 ->
+            oauth2.jwt { jwt ->
+                jwt.jwtAuthenticationConverter(
+                    ReactiveJwtAuthenticationConverterAdapter(CorePrincipalConverter())
+                )
+            }
+        }.build()
+
+    private fun permitAll(http: ServerHttpSecurity): SecurityWebFilterChain =
+        http.authorizeExchange {
+            it.anyExchange().permitAll()
+        }.build()
+
 }
