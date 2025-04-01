e39cc26b6594787aabfe04986cad457f59139ae7
Hknots <hknots99@gmail.com>
2025-04-01 11:59:36 +0200

    refactor: add jwtType, requiredScopes, requiredType

--- src/main/kotlin/no/fintlabs/resource/server/config/SecurityProperties.kt
+++ src/main/kotlin/no/fintlabs/resource/server/config/SecurityProperties.kt
@@ -1,5 +1,6 @@
 package no.fintlabs.resource.server.config
 
+import no.fintlabs.resource.server.JwtType
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
@@ -9,5 +10,8 @@ class SecurityProperties {
 
     var enabled: Boolean = true
     var issuerUri: String = "https://idp.felleskomponent.no/nidp/oauth/nam"
+    var jwtType: JwtType = JwtType.CORE
+    var requiredScopes: List<String>? = null
+    var requiredType: String? = null
 
 }
