fc95af48a6e5bada7b243a9f77dd98f22998a6ae
Hknots <hknots99@gmail.com>
2025-04-01 09:39:49 +0200

    feat: add SecurityProperties

---
+++ src/main/kotlin/no/fintlabs/resource/server/config/SecurityProperties.kt
@@ -0,0 +1,12 @@
+package no.fintlabs.resource.server.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties("fint.security")
+class SecurityProperties {
+
+    val enabled: Boolean = true
+
+}
