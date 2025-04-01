4f90733587904461ddd0062e297389db2c44e989
Hknots <hknots99@gmail.com>
2025-04-01 10:20:30 +0200

    refactor: add ConditionalOnProperty

--- src/main/kotlin/no/fintlabs/resource/server/config/JwtDecoderConfiguration.kt
+++ src/main/kotlin/no/fintlabs/resource/server/config/JwtDecoderConfiguration.kt
@@ -1,12 +1,14 @@
 package no.fintlabs.resource.server.config
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders
 
 @Configuration
+@ConditionalOnProperty(name = ["fint.security.enabled"], havingValue = "true")
 class JwtDecoderConfiguration(
     private val securityProperties: SecurityProperties
 ) {
