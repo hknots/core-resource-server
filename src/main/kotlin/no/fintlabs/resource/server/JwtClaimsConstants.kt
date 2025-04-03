aa074df80b5dc081fa0b32bc59a637765e327fc4
Hknots <hknots99@gmail.com>
2025-04-04 00:31:21 +0200

    refactor: remove unused constant FINT_ASSET_NAME from JwtClaimsConstants

--- src/main/kotlin/no/fintlabs/resource/server/JwtClaimsConstants.kt
+++ src/main/kotlin/no/fintlabs/resource/server/JwtClaimsConstants.kt
@@ -1,7 +1,6 @@
 package no.fintlabs.resource.server
 
 object JwtClaimsConstants {
-    const val FINT_ASSET_NAME = "fintAssetName"
     const val FINT_ASSET_IDS = "fintAssetIDs"
     const val ROLES = "Roles"
     const val SCOPE = "scope"
