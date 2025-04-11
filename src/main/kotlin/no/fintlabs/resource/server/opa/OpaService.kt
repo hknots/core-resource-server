1e3f8c1b37ef38b0cfa24ad6879d365444277f18
Hknots <hknots99@gmail.com>
2025-04-11 15:07:11 +0200

    chore: remove import

--- src/main/kotlin/no/fintlabs/resource/server/opa/OpaService.kt
+++ src/main/kotlin/no/fintlabs/resource/server/opa/OpaService.kt
@@ -1,6 +1,5 @@
 package no.fintlabs.resource.server.opa
 
-import kotlinx.coroutines.reactor.mono
 import no.fintlabs.resource.server.opa.model.OpaRequest
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
