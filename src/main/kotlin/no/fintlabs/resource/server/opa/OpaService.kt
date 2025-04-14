550999049dc0dc3df0be6c7c78ff14e76b754073
Hknots <hknots99@gmail.com>
2025-04-15 00:17:09 +0200

    refactor: remove unnecessary @Service annotation from OpaService class

--- src/main/kotlin/no/fintlabs/resource/server/opa/OpaService.kt
+++ src/main/kotlin/no/fintlabs/resource/server/opa/OpaService.kt
@@ -5,10 +5,8 @@ import no.fintlabs.resource.server.config.SecurityProperties
 import no.fintlabs.resource.server.opa.model.OpaRequest
 import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.security.oauth2.jwt.Jwt
-import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
-@Service
 class OpaService(
     private val securityProperties: SecurityProperties,
     private val opaClient: OpaClient
