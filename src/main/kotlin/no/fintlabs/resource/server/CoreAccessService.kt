3fdb139412ba29f615ff9473e0a3772e3509049b
Hknots <hknots99@gmail.com>
2025-04-15 00:04:04 +0200

    refactor: streamline authorizeCorePrincipal method for improved readability and efficiency

--- src/main/kotlin/no/fintlabs/resource/server/CoreAccessService.kt
+++ src/main/kotlin/no/fintlabs/resource/server/CoreAccessService.kt
@@ -3,27 +3,28 @@ package no.fintlabs.resource.server
 import no.fintlabs.resource.server.authentication.CorePrincipal
 import no.fintlabs.resource.server.config.SecurityProperties
 import no.fintlabs.resource.server.enums.FintType
+import no.fintlabs.resource.server.enums.JwtType
 import java.security.Principal
 
 class CoreAccessService(
     private val securityProperties: SecurityProperties
 ) {
 
-    fun authorizeCorePrincipal(principal: Principal): Boolean {
-        val corePrincipal = principal as CorePrincipal
+    fun authorizeCorePrincipal(principal: Principal) =
+        takeIf { securityProperties.jwtType == JwtType.CORE }?.let {
+            val corePrincipal = principal as CorePrincipal
 
-        val typeMatches = securityProperties.fintType?.let { requiredType ->
-            when (requiredType) {
-                FintType.CLIENT -> corePrincipal.isClient()
-                FintType.ADAPTER -> corePrincipal.isAdapter()
-            }
-        } ?: true
+            val typeMatches = securityProperties.fintType?.let { requiredType ->
+                when (requiredType) {
+                    FintType.CLIENT -> corePrincipal.isClient()
+                    FintType.ADAPTER -> corePrincipal.isAdapter()
+                }
+            } ?: true
 
-        val scopeMatches = securityProperties.requiredScopes?.any { requiredScope ->
-            corePrincipal.scopes.contains(requiredScope.formattedValue)
-        } ?: true
+            val scopeMatches = securityProperties.requiredScopes?.any { requiredScope ->
+                corePrincipal.scopes.contains(requiredScope.formattedValue)
+            } ?: true
 
-        return typeMatches && scopeMatches
-    }
-
+            return typeMatches && scopeMatches
+        } ?: false
 }
