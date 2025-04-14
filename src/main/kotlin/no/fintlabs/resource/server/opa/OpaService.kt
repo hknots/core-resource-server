489e28ac8801bc1d038fc799cd85c8b338a2e8c9
Hknots <hknots99@gmail.com>
2025-04-15 00:14:15 +0200

    refactor: rename authorizeCorePrincipal to isAuthorized for clarity and consistency

--- src/main/kotlin/no/fintlabs/resource/server/opa/OpaService.kt
+++ src/main/kotlin/no/fintlabs/resource/server/opa/OpaService.kt
@@ -1,5 +1,7 @@
 package no.fintlabs.resource.server.opa
 
+import kotlinx.coroutines.reactor.mono
+import no.fintlabs.resource.server.config.SecurityProperties
 import no.fintlabs.resource.server.opa.model.OpaRequest
 import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.security.oauth2.jwt.Jwt
@@ -8,11 +10,14 @@ import reactor.core.publisher.Mono
 
 @Service
 class OpaService(
+    private val securityProperties: SecurityProperties,
     private val opaClient: OpaClient
 ) {
 
     fun isAuthorized(jwt: Jwt, request: ServerHttpRequest): Mono<Boolean> =
-        opaClient.getDecision(OpaRequest.from(jwt, request)).map { it.result }
+        takeIf { securityProperties.opa }?.let {
+            opaClient.getDecision(OpaRequest.from(jwt, request)).map { it.result }
+        } ?: mono { true }
 
 
 }
