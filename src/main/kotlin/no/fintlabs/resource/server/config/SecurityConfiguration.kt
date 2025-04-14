4f0c6a3ee5f3621137b837f29bed5d42775c9ec2
Hknots <hknots99@gmail.com>
2025-04-15 00:17:25 +0200

    refactor: update security configuration to use OpaService with WebClient and streamline authorization logic

--- src/main/kotlin/no/fintlabs/resource/server/config/SecurityConfiguration.kt
+++ src/main/kotlin/no/fintlabs/resource/server/config/SecurityConfiguration.kt
@@ -3,22 +3,32 @@ package no.fintlabs.resource.server.config
 import no.fintlabs.resource.server.CoreAccessService
 import no.fintlabs.resource.server.converter.CorePrincipalConverter
 import no.fintlabs.resource.server.enums.JwtType
+import no.fintlabs.resource.server.opa.OpaClient
 import no.fintlabs.resource.server.opa.OpaService
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authorization.AuthorizationDecision
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.Authentication
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authorization.AuthorizationContext
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+import java.security.Principal
 
 @Configuration
 @EnableWebFluxSecurity
 class SecurityConfiguration(
     private val securityProperties: SecurityProperties,
-    private val opaService: OpaService
+    @Qualifier("opaWebClient") webClient: WebClient
 ) {
 
-    private val coreAccessService = CoreAccessService(securityProperties, opaService)
+    private val coreAccessService = CoreAccessService(securityProperties)
+    private val opaService = OpaService(securityProperties, OpaClient(webClient))
 
     @Bean
     fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain =
@@ -36,9 +46,21 @@ class SecurityConfiguration(
             exchanges.pathMatchers(*endpoints).permitAll()
         }
 
-        when (securityProperties.jwtType) {
-            JwtType.CORE -> exchanges.anyExchange().access(coreAccessService::authorizeCorePrincipal)
-            JwtType.DEFAULT -> exchanges.anyExchange().authenticated()
+        exchanges.anyExchange().access(this::authorizeRequest)
+    }
+
+    private fun authorizeRequest(
+        monoAuthentication: Mono<Authentication>,
+        authorizationContext: AuthorizationContext?
+    ): Mono<AuthorizationDecision> = monoAuthentication.flatMap { authentication ->
+        val principal = authentication as Principal
+        val request = authorizationContext!!.exchange.request
+
+        val corePrincipalAuthorized = coreAccessService.isAuthorized(principal)
+        val opaMono = opaService.isAuthorized(principal as Jwt, request)
+
+        opaMono.map { opaAuthorized ->
+            AuthorizationDecision(corePrincipalAuthorized && opaAuthorized)
         }
     }
 
