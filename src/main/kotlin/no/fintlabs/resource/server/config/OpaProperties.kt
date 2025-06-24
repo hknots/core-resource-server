d242f6f3e32c069f7233b0c8c30925da47b32ffb
Hknots <hknots99@gmail.com>
2025-06-24 22:18:21 +0200

    feat: add OpaProperties configuration and update OpaService to use it

---
+++ src/main/kotlin/no/fintlabs/resource/server/config/OpaProperties.kt
@@ -0,0 +1,13 @@
+package no.fintlabs.resource.server.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties("fint.security.opa")
+class OpaProperties {
+
+    var enabled: Boolean = false
+    var filter: Boolean = true
+
+}
--- src/main/kotlin/no/fintlabs/resource/server/opa/OpaService.kt
+++ src/main/kotlin/no/fintlabs/resource/server/opa/OpaService.kt
@@ -1,19 +1,19 @@
 package no.fintlabs.resource.server.opa
 
 import kotlinx.coroutines.reactor.mono
-import no.fintlabs.resource.server.config.SecurityProperties
+import no.fintlabs.resource.server.config.OpaProperties
 import no.fintlabs.resource.server.opa.model.OpaRequest
 import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.security.oauth2.jwt.Jwt
 import reactor.core.publisher.Mono
 
 class OpaService(
-    private val securityProperties: SecurityProperties,
+    private val opaProperties: OpaProperties,
     private val opaClient: OpaClient
 ) {
 
     fun isAuthorized(jwt: Jwt, request: ServerHttpRequest): Mono<Boolean> =
-        takeIf { securityProperties.opa }?.let {
+        takeIf { opaProperties.enabled }?.let {
             opaClient.getDecision(OpaRequest.from(jwt, request)).map { it.result }
         } ?: mono { true }
 
