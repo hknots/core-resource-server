93eea5a1cee19311de974c1ba617b71d66be2183
Hknots <hknots99@gmail.com>
2025-06-26 22:52:05 +0200

    feat: update OpaResponse to include Core data class with allow property

--- src/main/kotlin/no/fintlabs/resource/server/opa/model/OpaResponse.kt
+++ src/main/kotlin/no/fintlabs/resource/server/opa/model/OpaResponse.kt
@@ -1,6 +1,10 @@
 package no.fintlabs.resource.server.opa.model
 
 data class OpaResponse(
+    val result: Core = Core()
+)
+
+data class Core(
     val allow: Boolean = false,
     val fields: Set<String> = emptySet(),
     val relations: Set<String> = emptySet()
